ff6cf28c6419671efb01777b10eb6824ab2575ac
Masairo Obuchi <o.masahiro0328@gmail.com>
2022-07-28 02:19:16 +0900

    write Fill.asm (not solved)

--- projects/sec04/fill/Fill.asm
+++ projects/sec04/fill/Fill.asm
@@ -12,3 +12,120 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+// initialization
+@SCREEN
+D=A
+@nowscreen
+M=D
+@256
+D=A
+@rowcount
+M=D
+@32
+D=A
+@colcount
+M=D
+
+(LOOP)
+  @KBD
+  D=M
+  @WHITE
+  D;JEQ
+  @BLACK
+  D;JMP
+  @LOOP
+  0;JMP
+
+(WHITE)
+  // process all bit in register
+  // value in register & 0000000000000000
+  @nowscreen
+  A=M
+  0;JMP
+  D=M
+  @0
+  D=D&A
+  M=D
+  @colcount
+  M=M-1
+  D=M
+  @CMPCOLMAXWHITE
+  0;JMP
+
+(CMPCOLMAXWHITE)
+  @colcount
+  D=M
+  @RESETCOLWHITE
+  D;JEQ
+  @nowscreen
+  M=M+1
+  @WHITE
+  0;JMP
+
+// reset col count and decrement row count
+// if rowcount == 0, jump to reset row count
+(RESETCOLWHITE)
+  @16
+  D=A
+  @colcount
+  M=D
+  @rowcount
+  M=M-1
+  D=M
+  @RESETROW
+  D;JEQ
+  @WHITE
+  0;JMP
+
+(RESETROW)
+  @16
+  D=A
+  @rowcount
+  M=D
+  @LOOP
+  0;JMP
+
+(BLACK)
+  // process all bit in register
+  // value in register | 1111111111111111
+  @nowscreen
+  A=M
+  0;JMP
+  D=M
+  @1
+  D=D|A
+  M=D
+  @colcount
+  M=M-1
+  D=M
+  @CMPCOLMAXBLACK
+  0;JMP
+
+(CMPCOLMAXBLACK)
+  @colcount
+  D=M
+  @RESETCOLBLACK
+  D;JEQ
+  @nowscreen
+  M=M+1
+  @BLACK
+  0;JMP
+
+// reset col count and decrement row count
+// if rowcount == 0, jump to reset row count
+(RESETCOLBLACK)
+  @16
+  D=A
+  @colcount
+  M=D
+  @rowcount
+  M=M-1
+  D=M
+  @RESETROW
+  D;JEQ
+  @BLACK
+  0;JMP
+
+(END)
+  @END
+  0;JMP
