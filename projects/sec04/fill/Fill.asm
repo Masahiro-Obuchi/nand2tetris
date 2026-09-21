1553c30abdf3a2b4d62b4c8bb7772fac45c908b7
Masahiro Obuchi <o.masahiro0328@gmail.com>
2026-09-22 04:11:40 +0900

    Rewrite project4's solutions

--- projects/sec04/fill/Fill.asm
+++ projects/sec04/fill/Fill.asm
@@ -6,139 +6,57 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
-// initialization
-@SCREEN
-D=A
-@nowscreen
-M=D
-@256
-D=A
-@rowcount
-M=D
-@32
-D=A
-@colcount
-M=D
-
 (LOOP)
+  // Determine color
   @KBD
   D=M
   @WHITE
   D;JEQ
-  @BLACK
-  D;JMP
-  @LOOP
+
+  // key pressed, blacken the screen
+  @color
+  M=-1
+  @START_FILL
   0;JMP
 
 (WHITE)
-  // process all bit in register
-  // value in register & 0000000000000000
-  @nowscreen
-  A=M
-  D=M
-  @0
-  D=D&A
-  @nowscreen
-  A=M
-  M=D
+  @color
+  M=0
 
-  @nowscreen
-  M=M+1
-
-  @colcount
-  M=M-1
-  D=M
-  @CMPCOLMAXWHITE
-  0;JMP
-
-(CMPCOLMAXWHITE)
-  @colcount
-  D=M
-  @RESETCOLWHITE
-  D;JEQ
-  @WHITE
-  0;JMP
-
-// reset col count and decrement row count
-// if rowcount == 0, jump to reset row count
-(RESETCOLWHITE)
-  @32
-  D=A
-  @colcount
-  M=D
-  @rowcount
-  M=M-1
-  D=M
-  @RESETROW
-  D;JEQ
-  @WHITE
-  0;JMP
-
-(RESETROW)
-  @256
-  D=A
-  @rowcount
-  M=D
-
+(START_FILL)
+  // ptr = SCREEN
   @SCREEN
   D=A
-  @nowscreen
+  @ptr
   M=D
 
-  @LOOP
-  0;JMP
-
-(BLACK)
-  // process all bit in register
-  // value in register | 1111111111111111
-  @nowscreen
-  A=M
+(FILL)
+  // RAM[ptr] = color
+  @color
   D=M
-  @0
-  D=D&A
-  D=!D
-  @nowscreen
+  @ptr
   A=M
   M=D
 
-  @nowscreen
+  // ptr = ptr + 1
+  @ptr
   M=M+1
 
-  @colcount
-  M=M-1
-  D=M
-  @CMPCOLMAXBLACK
-  0;JMP
-
-(CMPCOLMAXBLACK)
-  @colcount
-  D=M
-  @RESETCOLBLACK
-  D;JEQ
-  @BLACK
-  0;JMP
-
-// reset col count and decrement row count
-// if rowcount == 0, jump to reset row count
-(RESETCOLBLACK)
-  @32
+  // if ptr < KBD, continue filling
+  // KBD address is located immediately after the screen memory
+  @KBD
   D=A
-  @colcount
-  M=D
-  @rowcount
-  M=M-1
-  D=M
-  @RESETROW
-  D;JEQ
-  @BLACK
-  0;JMP
+  @ptr
+  D=M-D
+  @FILL
+  D;JLT
 
-(END)
-  @END
+  // Check keyboard again
+  @LOOP
   0;JMP
--- projects/sec04/mult/Mult.asm
+++ projects/sec04/mult/Mult.asm
@@ -10,26 +10,42 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
-@R2
-M=0
-@R1
-D=M
-@count
-M=D
-@END
-D;JEQ
+  // i = 1
+  @i
+  M=1
+  // sum = 0
+  @sum
+  M=0
+
 (LOOP)
+  // if (i > R1) goto STOP
+  @i
+  D=M
+  @R1
+  D=D-M
+  @STOP
+  D;JGT
+  // sum = sum + R0
+  @sum
+  D=M
   @R0
-  D=M
-  @R2
-  M=M+D
-  @count
-  M=M-1
-  D=M
-  @END
-  D;JEQ
+  D=D+M
+  @sum
+  M=D
+  // i = i + 1
+  @i
+  M=M+1
+  // goto LOOP
   @LOOP
   0;JMP
+
+(STOP)
+  // R2 = sum
+  @sum
+  D=M
+  @R2
+  M=D
+
 (END)
   @END
   0;JMP
